13a9b2990f8c945fec234fb6e11eab3a52f323a4
Jamal <jamal@hku.hk>
2017-10-20 01:04:12 +0800

    all pages except login signup page added with templating

--- src/tutoria/mainApp/urls.py
+++ src/tutoria/mainApp/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^index$', views.index, name='index'),
-    url(r'^search$', views.search, name='search')
+    url(r'^search$', views.search, name='search'),
+    url(r'^profile$', views.profile, name='profile'),
+    url(r'^bookings$', views.bookings, name='bookings'),
+    url(r'^wallet$', views.wallet, name='wallet'),
+    url(r'^book$', views.book, name='book'),
+    url(r'^confirmation$', views.confirmation, name='confirmation'),
 ]
--- src/tutoria/mainApp/views.py
+++ src/tutoria/mainApp/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
-    return render(request, 'mainApp\landing.html')
+    return render(request, 'mainApp/landing.html')
 
-
+@csrf_exempt
 def search(request):
-    return render(request, 'mainApp\search.html')
+    return render(request, 'mainApp/search.html')
+
+@csrf_exempt
+def profile(request):
+    return render(request, 'mainApp/profile.html')
+
+@csrf_exempt
+def bookings(request):
+    return render(request, 'mainApp/bookings.html')
+
+@csrf_exempt
+def wallet(request):
+    return render(request, 'mainApp/wallet.html')
+
+@csrf_exempt
+def book(request):
+    return render(request, 'mainApp/book.html')
+
+@csrf_exempt
+def confirmation(request):
+    return render(request, 'mainApp/confirmation.html')
