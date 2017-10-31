f6d1aa504f65bb5338323a13bc56a32b889065d7
Jamal <jamal@hku.hk>
2017-10-31 21:42:09 +0800

    better search page

--- src/tutoria/mainApp/templatetags/custom_tags.py
+++ src/tutoria/mainApp/templatetags/custom_tags.py
@@ -6,3 +6,8 @@ import math
 def mult(value, arg):
     return math.ceil(int(value)*float(arg))
 register.filter('mult', mult)
+
+@register.filter
+def firstname(value):
+    return value.split(' ')[0]
+register.filter('firstname', firstname)
